49c12d38594e4ea0435b7211e307a047fd5ca681
Moosa Khan <contact.moosakhan@gmail.com>
2025-11-18 13:27:48 -0500

    feat(admin): add Admin page (issue #39)

--- Project/goosemarket-frontend/src/main.jsx
+++ Project/goosemarket-frontend/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import Leaderboard from "./pages/Leaderboard.jsx";
+import Admin from "./pages/Admin.jsx";
 
 import CreateEvent from "./pages/CreateEvent.jsx";
 
@@ -62,6 +63,15 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </Layout>
   }
 />
+<Route
+  path="/admin"
+  element={
+    <Layout>
+      <Admin />
+    </Layout>
+  }
+/>
+
 
 
           </Routes>
